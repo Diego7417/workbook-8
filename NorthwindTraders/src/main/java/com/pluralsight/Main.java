0106bc3547d7e8205bb756b11cd8365b38b033ab
Diego7417 <Dangeles99@gmail.com>
2025-06-10 09:12:55 -0400

    NorthwindTraders

---
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        // Optional: Uncomment if the driver still isn't loading
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // Connect to Northwind DB
+        String url = "jdbc:mysql://localhost:3306/northwind";
+        String username = "root"; // use your actual MySQL username
+        String password = "yearup"; // use your actual password
+
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        Statement statement = connection.createStatement();
+
+        // Query all product names
+        String query = "SELECT ProductName FROM Products";
+        ResultSet results = statement.executeQuery(query);
+
+        // Print each product name
+        while (results.next()) {
+            String productName = results.getString("ProductName");
+            System.out.println(productName);
+        }
+
+        // Cleanup
+        results.close();
+        statement.close();
+        connection.close();
+    }
+}
